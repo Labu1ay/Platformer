4db3ad1692277201da155d67edcf9ee6b03344be
Alexander <102901721+Labu1ay@users.noreply.github.com>
2022-09-11 16:38:25 +0300

    Update PlayerMove.cs

--- Platformer/Assets/Scripts/PlayerMove.cs
+++ Platformer/Assets/Scripts/PlayerMove.cs
@@ -4,16 +4,42 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
+    public Rigidbody Rigidbody;
     public float MoveSpeed;
     public float JumpSpeed;
     public float Friction;
     public bool Grounded;
 
-    public Rigidbody Rigidbody;
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (Grounded)
+            {
+                Rigidbody.AddForce(0f, JumpSpeed, 0f, ForceMode.VelocityChange);
+            }
+        }
+    }
     void FixedUpdate()
     {
         Rigidbody.AddForce(Input.GetAxis("Horizontal") * MoveSpeed, 0f, 0f, ForceMode.VelocityChange);
 
         Rigidbody.AddForce(-Rigidbody.velocity.x * Friction, 0f, 0f, ForceMode.VelocityChange);
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        for (int i = 0; i < collision.contactCount; i++)
+        {
+            float angleToNormal = Vector3.Angle(collision.contacts[i].normal, Vector3.up);
+            if (angleToNormal < 45f)
+            {
+                Grounded = true;
+            }
+        }
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        Grounded = false;
     }
 }
